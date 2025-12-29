f735c4583946b750bb333c131ba4daf0e7b35dc1
natashaDian <natashadiann.m@gmail.com>
2025-12-29 18:34:32 +0700

    adding console and monitor output

--- spark/dag/spark_jobs/kafka_transaction_validation.py
+++ spark/dag/spark_jobs/kafka_transaction_validation.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (
-    from_json, col, when, lit, concat_ws, to_timestamp
+    from_json, col, when, lit, concat_ws, to_timestamp, current_timestamp, window, sum as _sum, expr
 )
 from pyspark.sql.types import (
     StructType, StructField, StringType, IntegerType
 )
+from datetime import datetime
 
 spark = SparkSession.builder \
     .appName("KafkaTransactionValidation") \
@@ -51,10 +52,13 @@ range_check = col("amount").between(1, 10_000_000)
 source_check = col("source").isin("mobile", "web", "pos")
 
 parsed_df = parsed_df.withColumn(
-    "event_ts",
-    to_timestamp("timestamp")
+    "event_time",
+    to_timestamp("timestamp")   
 )
-timestamp_check = col("event_ts").isNotNull()
+timestamp_check = col("event_time").isNotNull()
+
+processing_time = current_timestamp()
+late_check = col("event_time") < (processing_time - expr("INTERVAL 3 MINUTES"))
 
 validated_df = parsed_df.withColumn(
     "error_reason",
@@ -64,7 +68,8 @@ validated_df = parsed_df.withColumn(
         when(~type_check, "INVALID_TYPE"),
         when(~range_check, "AMOUNT_OUT_OF_RANGE"),
         when(~source_check, "INVALID_SOURCE"),
-        when(~timestamp_check, "INVALID_TIMESTAMP")
+        when(~timestamp_check, "INVALID_TIMESTAMP"),
+        when(late_check, "LATE_EVENT_GT_3_MIN")
     )
 )
 validated_df = validated_df.withColumn(
@@ -75,8 +80,44 @@ valid_df = validated_df.filter(col("is_valid") == True)
 invalid_df = validated_df.filter(col("is_valid") == False)
 
 valid_df = valid_df \
-    .withWatermark("event_ts", "5 minutes") \
+    .withWatermark("event_time", "3 minutes") \
     .dropDuplicates(["event_id"])
+
+windowed_df = valid_df.groupBy(
+    window(col("event_time"), "1 minute")
+).agg(
+    _sum(lit(1)).alias("txn_count")
+)
+
+running_total = {"count": 0}
+
+def write_console(batch_df, batch_id):
+    global running_total
+
+    if batch_df.rdd.isEmpty():
+        return   # <-- INI PENTING
+
+    batch_count = batch_df.agg(
+        _sum("txn_count").alias("c")
+    ).collect()[0]["c"] or 0
+
+    running_total["count"] += int(batch_count)
+
+    out = spark.createDataFrame(
+        [(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+          running_total["count"])],
+        ["timestamp", "running_total"]
+    )
+
+    out.show(truncate=False)
+
+monitor_query = windowed_df.writeStream \
+    .foreachBatch(write_console) \
+    .outputMode("update") \
+    .option("checkpointLocation", "/tmp/checkpoint_monitor") \
+    .trigger(processingTime="5 seconds") \
+    .start()
+
 
 valid_query = valid_df.selectExpr(
     "CAST(user_id AS STRING) AS key",
